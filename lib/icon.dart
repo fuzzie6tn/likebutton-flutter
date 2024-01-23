486051ad06feb06524521408c2c18a46b461266e
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2024-01-23 15:41:24 +0500

    push

---
+++ lib/icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:like_button/like_button.dart';
+
+class MyIcon extends StatelessWidget {
+  final int number = 0;
+  const MyIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      // size:80;
+      likeCount: number;
+      countPosition: CountPostion.bottom;
+      likeBuilder: (isTapped){
+        return Icon(
+          Icons.p,
+          color: isTapped? Colors.purple: Colors.grey,
+          s
+        )
+      }
+    );
+  }
+}
