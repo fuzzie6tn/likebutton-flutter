230a7d899d05313f07136bb32d112ce48b1a52d2
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2024-01-24 16:23:40 +0500

    push

--- lib/icon.dart
+++ lib/icon.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:like_button/like_button.dart';
+// import 'package:like_button/like_button.dart';
 
 class MyIcon extends StatelessWidget {
   final int number = 0;
@@ -9,7 +9,7 @@ class MyIcon extends StatelessWidget {
   Widget build(BuildContext context) {
     return Icon(
       // size:80;
-      likeCount: number;
+      // likeCount: number;
       countPosition: CountPostion.bottom;
       likeBuilder: (isTapped){
         return Icon(
