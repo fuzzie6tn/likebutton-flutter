593fbf2876d2b518ba8fc18ae482129c2855ddd0
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2024-01-27 00:26:13 +0500

    push

---
+++ flutter-codes/practice_codes/dart_application_1/lib/ConditionsandLoops/1.conditions.dart
@@ -0,0 +1,17 @@
+void main() {
+  int weekday = 3;
+
+  switch (weekday) {
+    case 1:
+      print("monday");
+      break;
+    case 2:
+      print("tuesday");
+      break;
+    case 3:
+      print("Wed");
+      break;
+    default:
+      print("Invalid!");
+  }
+}
---
+++ flutter-codes/practice_codes/dart_application_1/lib/ConditionsandLoops/3.enum.dart
@@ -0,0 +1,22 @@
+//An enum or enumeration is used for defining value according to you.
+
+enum Weather { rainy, sun, cloudy, snowy }
+
+void main() {
+  const weather = Weather.snowy;
+  switch (weather) {
+    case Weather.snowy:
+      print("snowy");
+      break;
+
+    case Weather.sun:
+      print("get you glasses");
+      break;
+    case Weather.rainy:
+      print("Get your ambrella");
+      break;
+    default:
+      print("Oops looks like you chose the wrong case!");
+      break;
+  }
+}
