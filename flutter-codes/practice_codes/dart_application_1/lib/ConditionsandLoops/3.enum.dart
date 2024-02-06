83bafa9fad7541dc6c54e76812286700b7bd639d
Fazila Roshan Beg <fazilaroushanbeg@gmail.com>
2024-02-06 17:02:43 +0500

    push

--- flutter-codes/practice_codes/dart_application_1/lib/ConditionsandLoops/3.enum.dart
+++ flutter-codes/practice_codes/dart_application_1/lib/ConditionsandLoops/3.enum.dart
@@ -10,7 +10,7 @@ void main() {
       break;
 
     case Weather.sun:
-      print("get you glasses");
+      print("get you glasses \n \n");
       break;
     case Weather.rainy:
       print("Get your ambrella");
